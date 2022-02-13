c26cce6e36bfecbb545c32b1b077280a9a3dea87
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-02-13 15:06:47 -0300

    Finaliza ex. 1

---
+++ course_15A/bloco_22/dia_2/cha_exercises/exercise1.js
@@ -0,0 +1,20 @@
+const funcPromise = async (num1, num2, num3) => {
+  const promise = new Promise((resolve, reject) => {
+    if (typeof(num1) != 'number' || typeof(num2) != 'number' || typeof(num3) != 'number')
+      reject(new Error  ('Informe apenas números'));
+    
+      const resultado = (num1 + num2) * num3;
+    
+    if (resultado < 50)
+      reject(new Error('Valor muito baixo'));
+
+      resolve(resultado);
+    });
+  return promise;
+};
+
+funcPromise('a', 2, 3)
+  .then((result) => console.log('Sucesso:', {result}))
+  .catch((err) => console.log(err.message))
+
+
---
+++ course_15A/bloco_22/dia_2/cha_exercises/testeIf.js
@@ -0,0 +1,9 @@
+const operacao = (num1, num2) => {
+  const resultado = num1*num2;
+  if (num1 > 5) 
+  console.log(resultado);
+
+  else console.log('num1 mt baixo')
+}
+
+operacao(5,2);
