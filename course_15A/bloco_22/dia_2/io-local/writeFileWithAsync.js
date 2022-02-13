20b015581fa9c41469b57b84719346f8ebdc5934
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-02-13 11:20:38 -0300

    Testa a flag 'wx' no writeFile do modulo 'fs'

--- course_15A/bloco_22/dia_2/io-local/writeFileWithAsync.js
+++ course_15A/bloco_22/dia_2/io-local/writeFileWithAsync.js
@@ -2,10 +2,10 @@ const fs = require('fs').promises;
 
 const escreveArq = async () => {
   try {
-    await fs.writeFile('./meu-arquivo.txt', 'Hello, mundão de meu deus!');
+    await fs.writeFile('./meu-novo-arquivo.txt', 'Eu estive aqui :eyes:', { flag: 'wx'});
     console.log('Arquivo escrito com sucesso! =D');
   } catch (err) {
-    console.log('Erro ao escrever no arquivo', err.message);
+    console.log('Erro ao escrever no arquivo', err);
   }
 }
 
