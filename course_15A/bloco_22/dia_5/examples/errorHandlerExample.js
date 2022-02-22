f7eaec44b0a165cd37f8614544e5e46f89f962ea
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-02-22 19:15:48 -0300

    Refatora o codigo da example error com o rescue

--- course_15A/bloco_22/dia_5/examples/errorHandlerExample.js
+++ course_15A/bloco_22/dia_5/examples/errorHandlerExample.js
@@ -4,16 +4,12 @@ const rescue = require('express-rescue');
 
 const app = express();
 
-app.get('/:fileName', rescue(async (req, res, next) => {
-    try {
+app.get('/:fileName', rescue(async (req, res) => {
         const file = await fs.readFile(`./${req.params.fileName}`);
         res.send(file.toString('utf-8'));
-    } catch (e) {
-        next(e);
-    }
 }));
 
-app.use(function (err, req, res, next) {
+app.use(function (err, _req, res, _next) {
   res.status(500).json({ error: `Erro: ${err.message}` });
 });
 
