ab67e563d19934a720ca7d645095b8b8d6392631
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-03-05 11:28:14 -0300

    Finaliza o exercicio exemplo da sessao praticando

--- course_15A/bloco_23/dia_2/example_exercises/hello-msc/index.js
+++ course_15A/bloco_23/dia_2/example_exercises/hello-msc/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const Author = require('./controllers/Authors');
-
+const Author = require('./controllers/Author');
 
 const app = express();
 
@@ -12,10 +11,10 @@ app.get('/authors', Author.getAll);
 
 app.get('/authors/:id', Author.findById);
 
-app.post('/authors', Author.createAuthor);
+app.post('/authors', Author.create);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log('Ouvindo na porta:', PORT)
+  console.log(`Ouvindo a porta ${PORT}`);
 });
--- course_15A/bloco_23/dia_2/example_exercises/hello-msc/models/connection.js
+++ course_15A/bloco_23/dia_2/example_exercises/hello-msc/models/connection.js
@@ -1,9 +1,11 @@
+// hello-msc/models/connection.js
+
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '3G1pytP$yc#',
+  password: 'sua_senha',
   database: 'model_example'
 });
 
