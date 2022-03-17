317a2f10084dcdf9fd596307bbeda7a737c1307f
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-03-17 08:04:15 -0300

    Altera a funçao book no models

--- course_15A/bloco_24/dia_1/cha_exercises/books-api-sequelize/models/Book.js
+++ course_15A/bloco_24/dia_1/cha_exercises/books-api-sequelize/models/Book.js
@@ -1,13 +1,15 @@
 const { DataTypes } = require("sequelize/types");
 
 const Book = (sequelize, Datatypes) => {
-  const User = sequelize.define("User", {
+  const Book = sequelize.define("Book", {
     id:Datatypes.INTEGER,
     title: Datatypes.STRING,
     author: DataTypes.STRING,
     pageQuantity: DataTypes.INTEGER,
     createdAt: DataTypes.DATE,
   });
+  
+  return Book;
 };
 
-module.exports = User;
+module.exports = Book;
