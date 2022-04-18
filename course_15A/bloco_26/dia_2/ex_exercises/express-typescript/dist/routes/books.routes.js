c5ea0a2e0d7c44ec07d29a6a4f83354205e42916
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-04-18 19:50:05 -0300

    Finaliza o exercicio exemplo do dia 26.3

---
+++ course_15A/bloco_26/dia_2/ex_exercises/express-typescript/dist/routes/books.routes.js
@@ -0,0 +1,12 @@
+"use strict";
+// ./routes/books.routes.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const books_controller_1 = __importDefault(require("../controllers/books.controller"));
+const router = (0, express_1.Router)();
+const booksController = new books_controller_1.default();
+router.get('/', booksController.getAll);
+exports.default = router;
