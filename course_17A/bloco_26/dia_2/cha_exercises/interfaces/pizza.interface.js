a56990d88b02ef0a5764c61c260a7da6fcec0dfc
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-05-31 11:27:00 -0300

    Finaliza exercicio 4

--- course_17A/bloco_26/dia_2/cha_exercises/interfaces/pizza.interface.js
+++ course_17A/bloco_26/dia_2/cha_exercises/interfaces/pizza.interface.js
@@ -11,6 +11,28 @@ const Nutela = {
     flavor: 'Nutela',
     slices: 4
 };
-console.log(Calabresa);
-console.log(Marguerita);
-console.log(Nutela);
+const pizzaComum1 = {
+    flavor: 'Calabresa',
+    slices: 8,
+};
+const pizzaComum2 = {
+    flavor: 'Frango',
+    slices: 6,
+};
+const pizzaComum3 = {
+    flavor: 'Pepperoni',
+    slices: 4,
+};
+const pizzaVegetariana1 = {
+    flavor: 'Palmito',
+    slices: 8,
+};
+const pizzaVegetariana2 = {
+    flavor: 'Cogumelo',
+    slices: 6,
+};
+const pizzaDoce = {
+    flavor: 'Goiabada com Queijo',
+    slices: 4,
+};
+console.log(pizzaDoce);
--- course_17A/bloco_26/dia_2/cha_exercises/interfaces/pizza.interface.ts
+++ course_17A/bloco_26/dia_2/cha_exercises/interfaces/pizza.interface.ts
@@ -1,3 +1,6 @@
+type FlavorsComum = 'Calabresa' | 'Frango' | 'Pepperoni';
+type FlavorsVegetariana = 'Marguerita' | 'Palmito' | 'Cogumelo';
+type FlavorsDoce = 'Nutela' | 'Goiabada com Queijo' | 'Mashmallow'
 type Slices = 4 | 6 | 8;
 
 interface Pizza {
@@ -20,6 +23,53 @@ const Nutela: Pizza = {
   slices: 4
 }
 
-console.log(Calabresa);
+/* console.log(Calabresa);
 console.log(Marguerita);
-console.log(Nutela);
+console.log(Nutela); */
+
+interface Comum extends Pizza {
+  flavor: FlavorsComum;
+  slices: Slices;
+}
+
+interface Vegetariana extends Pizza {
+  flavor: FlavorsVegetariana;
+  slices: Slices;
+}
+
+interface Doce extends Pizza {
+  flavor: FlavorsDoce;
+  slices: Slices;
+}
+
+const pizzaComum1: Comum = {
+  flavor: 'Calabresa',
+  slices: 8,
+}
+
+const pizzaComum2: Comum = {
+  flavor: 'Frango',
+  slices: 6,
+}
+
+const pizzaComum3: Comum = {
+  flavor: 'Pepperoni',
+  slices: 4,
+}
+
+const pizzaVegetariana1: Vegetariana = {
+  flavor: 'Palmito',
+  slices: 8,
+}
+
+const pizzaVegetariana2: Vegetariana = {
+  flavor: 'Cogumelo',
+  slices: 6,
+}
+
+const pizzaDoce: Doce = {
+  flavor: 'Goiabada com Queijo',
+  slices: 4,
+}
+
+console.log(pizzaDoce);
