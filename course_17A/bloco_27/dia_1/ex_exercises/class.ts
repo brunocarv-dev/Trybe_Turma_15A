63db630b8efdf5e32748a57a461a81c62d64894a
brunocarv-dev <bruno.fox_carvalho@hotmail.com>
2022-06-23 13:48:42 -0300

    Finaliza o exerc 2

--- course_17A/bloco_27/dia_1/ex_exercises/class.ts
+++ course_17A/bloco_27/dia_1/ex_exercises/class.ts
@@ -1,14 +1,43 @@
-export default class Student {
-  private _enrollment: string;
-  private _name: string;
+class Person {  
+  constructor (private _name: string,private _birthDate: Date) {
+    this.name = _name;
+    this.birthDate = _birthDate;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+  
+  public set name(value: string) {
+    if (value.length < 4 ) {
+      throw new Error('O nome deve ter no mínimo três caracteres');
+    }
+    this._name = value;
+  }
+
+  public get birthDate(): Date {
+    return this._birthDate;
+  }
+
+  public set birthDate(value: Date) {
+    this.validateBirthDate(value);
+    this._birthDate = value;
+  }
+
+  public validateBirthDate(value: Date): void {
+    if (value.getTime() > new Date().getTime()) throw new Error('A data de nascimento não pode ser uma data no futuro');
+    if ((new Date().getTime() - value.getTime()) / 31_536_000_000 > 120) throw new Error('A pessoa não pode possuir mais de 120 anos')
+  }
+}
+
+export default class Student extends Person {
+  private _enrollment: string = String();
   private _examsGrades: number[] = [];
   private _worksGrades: number[] = [];
   
-  constructor(enrollment: string, name: string, examsGrades: number[], worksGrades: number[]) {
-    this._enrollment = enrollment;
-    this._name = name;
-    this.examsGrades = examsGrades;
-    this.worksGrades = worksGrades;
+  constructor(name: string, birthDate: Date) {
+    super(name, birthDate);
+    this.enrollment = this.generateEnrollment();
   }
   
   public get enrollment(): string {
@@ -16,15 +45,8 @@ export default class Student {
   }
   
   public set enrollment(value: string) {
+    if (value.length < 16) throw new Error('A matrícula deve possuir no mínimo 16 caracteres.');
     this._enrollment = value;
-  }
-  
-  public get name(): string {
-    return this._name;
-  }
-  
-  public set name(value: string) {
-    this._name = value;
   }
   
   public get examsGrades(): number[] {
@@ -64,45 +86,20 @@ export default class Student {
     const divider = this.examsGrades.length + this.worksGrades.length;
     return Math.round(sumGrades / divider);
   }
-}
 
-class Person {  
-  constructor (private _name: string,private _birthDate: Date) {
-    this.name = _name;
-    this.birthDate = _birthDate;
-  }
+  generateEnrollment(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
 
-  public get name(): string {
-    return this._name;
-  }
-  
-  public set name(value: string) {
-    if (value.length < 4 ) {
-      throw new Error('O nome deve ter no mínimo três caracteres');
-    }
-    this._name = value;
-  }
-
-  public get birthDate(): Date {
-    return this._birthDate;
-  }
-
-  public set birthDate(value: Date) {
-    this.validateBirthDate(value);
-    this._birthDate = value;
-  }
-
-  public validateBirthDate(value: Date): void {
-    if (value.getTime() > new Date().getTime()) throw new Error('A data de nascimento não pode ser uma data no futuro');
-    if ((new Date().getTime() - value.getTime()) / 31_536_000_000 > 120) throw new Error('A pessoa não pode possuir mais de 120 anos')
+    return `STU${randomStr}`;
   }
 }
 
-const student1 = new Student('1234', 'Gustavo', [5, 6, 7, 8], [8, 9]);
+
+const student1 = new Student('Gustavo', new Date('2005/03/17'));
 console.log(student1);
 console.log('Media das notas', student1.sumAverage());
 
-const student2 = new Student('12345', 'João', [7, 9, 10, 8], [8, 10]);
+const student2 = new Student('João', new Date('2006/07/10'));
 console.log(student2);
 console.log('Media das notas', student2.sumAverage());
 
